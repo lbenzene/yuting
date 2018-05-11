f0ad463b240649680556162dc1a7e23c03a890fd
Arcane <lbenzene@qq.com>
2018-05-11 12:59:36 +0800

    5/11 1

--- js/main.js
+++ js/main.js
@@ -7,29 +7,44 @@ $user.good = 0;
 
 var NowAct = 1;
 
-function next_act() {
-  $("#step" + NowAct).slideUp(1000, function(){
-    $("#step" + (NowAct+1)).fadeIn();
-  })
-}
+
 
 $("#step1 a.btn").click(function(e){
   e.stopPropagation();
   $user.name = $("input[name='name']").val();
   $user.profession = $("input[name='major']:checked").val();
-  next_act();
-  NowAct = 2;
-  console.log(NowAct);
+  change1();
+  next_act(2);
 })
 
 $("#step2 a.choice").click(function(e) {
   e.stopPropagation()
   if ($(this).data("ans")) {
-    $("#step2 div.text").fadeOut();
-    $("#step2 div.result").fadeIn();
+    $("#step2 div.text").fadeOut(function(){
+      $("#step2 div.result").fadeIn();
+
+      $("#step2 div.result a.btn").click(function(e){
+        e.stopPropagation();
+        next_act(3);
+      })
+    })
   } else {
-    next_act();
-    NowAct = 3;
-      console.log(NowAct);
+    next_act(3);
   }
 })
+
+function next_act(n) {
+  NowAct = n;
+  $("#main-warpper").animate({top:'-' + (n-1) + '00%'}, "slow");
+  // $("#step" + NowAct).slideUp(1000, function(){
+  //   $("#step" + (NowAct+1)).fadeIn();
+  // })
+}
+
+function change1() {
+  if ($user.profession == "文") {
+    $("#step3-pro").text("文学院的严教授")
+  } else {
+    $("#step3-pro").text("理学院的陈教授")
+  }
+}
